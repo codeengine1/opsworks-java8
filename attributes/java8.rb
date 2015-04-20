5879b7ca9eab523a81b18b7943a26a5d369f4ffb
David Maple <d@davemaple.com>
2015-04-20 13:36:22 -0400

    accept oracle terms

--- attributes/java8.rb
+++ attributes/java8.rb
@@ -1,3 +1,3 @@
 override[:java][:install_flavor] = "oracle"
 override[:java][:jdk_version] = "8"
-override[:java][:accept_oracle_download_terms] = true
+override[:java][:oracle][:accept_oracle_download_terms] = true
