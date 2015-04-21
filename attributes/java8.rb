12a67d01eae6810e9eaeee942872124c6ee0cb3a
David Maple <d@davemaple.com>
2015-04-21 10:09:35 -0400

    jce unlimited

--- attributes/java8.rb
+++ attributes/java8.rb
@@ -1,3 +1,4 @@
 override[:java][:install_flavor] = "oracle"
 override[:java][:jdk_version] = "8"
 override[:java][:oracle][:accept_oracle_download_terms] = true
+override[:java][:oracle][:jce][:enabled] = true
